b5419bc24a1e9b4445bf8c4d301b4448454ea4b1
hanuman12511 <er.hanumansaini@gmail.com>
2023-10-10 21:21:00 +0530

    dfsfs

--- ciya/myciya/views.py
+++ ciya/myciya/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
 # Create your views here.
 def login(req):
     context={}
+    if(req.method=="POST"):
+        print("login",req.POST.get('email'))
+        return redirect("register")
     return render(req,"Login/Login.html",context) 
 def register(req):
     context={}
     return render(req,"register/register.html",context) 
+
+    
+def handlelogin(req):
+    return HttpResponse("login successfully")
 
 
 # Login is foldername
