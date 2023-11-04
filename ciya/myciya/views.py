60c0bc789f90dcb9dad10c0019e027a3f3318717
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-11-04 21:46:03 +0530

    ggg

--- ciya/myciya/views.py
+++ ciya/myciya/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from myciya.models import *
 # Create your views here.
 def login(req):
     context={}
     if(req.method=="POST"):
         print("login",req.POST.get('email'))
+        user1 =user()
+        user1.uid = req.POST.get('email')
+        user1.save()
         if req.POST.get('email') =="admin@gmail.com":
             return redirect("home")
         else :
             return redirect("login")
     return render(req,"Login/Login.html",context) 
 def register(req):
-    context={}
+    
+    form = user.objects.all()
+    print(form)
+    context = {'form':form}
+    
     return render(req,"register/register.html",context) 
 
     
